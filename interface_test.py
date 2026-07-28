51c3a3c6a2acde4354ee7d2df9e8b67dd9885c76
Liliana Holguin <lilianajholguin@gmail.com>
2026-07-28 09:16:52 -0600

    Update interface_test.py

--- interface_test.py
+++ interface_test.py
@@ -20,14 +20,14 @@ label.pack()
 def disable():
     with ConnectHandler(**cisco_switch) as net_connect:
         net_connect.enable()
-        commands = ['interface Gi5/0/36', 'shutdown']
+        commands = ['interface GiX', 'shutdown']
         net_connect.send_config_set(commands)
         print("Disabled")
 
 def enable():
     with ConnectHandler(**cisco_switch) as net_connect:
         net_connect.enable()
-        commands = ['interface Gi5/0/36', 'no shutdown']
+        commands = ['interface GiX', 'no shutdown']
         net_connect.send_config_set(commands)
         print("Enabled")
 
